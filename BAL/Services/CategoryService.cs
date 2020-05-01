e2d588412fbac3dbe683850b3af762410aa93250
Nikita20697 <64461539+Nikita20697@users.noreply.github.com>
2020-05-01 12:15:04 -0400

    Manage Medicine Module

--- BAL/Services/CategoryService.cs
+++ BAL/Services/CategoryService.cs
@@ -62,28 +62,10 @@ namespace BAL.Services
 
         public Category UpdateCategory(Category categoryEntity)
         {
-            //categoryData.CategoryID = categoryEntity.CategoryID;
-            //categoryData.name = categoryEntity.name;
-            //categoryData.isActive = categoryEntity.isActive;
-            //categoryData.isDeleted = categoryEntity.isDeleted;
-            //categoryData.updatedOn = categoryEntity.updatedOn;
-            //db.SaveChanges();
-
-
             context.Entry(categoryEntity).State = EntityState.Modified; 
             context.SaveChanges();
 
             return categoryEntity;
-            // Verify weather user inserted(actually updated) or not.
-            //Category presentCategory = context.Categories.SingleOrDefault(c => c.Id == categoryEntity.Id);
-            //if (presentCategory != null)
-            //{
-            //    return presentCategory;
-            //}
-            //else
-            //{
-            //    return null;
-            //}
         }
 
 
@@ -94,11 +76,6 @@ namespace BAL.Services
         /// <returns>Category</returns>
         public virtual Category GetCategoryById(int Id)
         {
-            //ModelContext db = new ModelContext();
-            //Category objCategory = new Category();
-            //objCategory = db.Categorys.Where(a => a.CategoryID == CategoryID).FirstOrDefault();
-            //return objCategory;
-
             var query = from c in context.Categories where c.Id == Id select c;
             var category = query.FirstOrDefault();
             var model = new Category()
--- BAL/Services/IMedicineService.cs
+++ BAL/Services/IMedicineService.cs
@@ -8,8 +8,8 @@ namespace BAL.Services
         public IEnumerable<Medicine> GetAllMedicines();
         public IEnumerable<Medicine> GetMedicineByName(string MedicineName);
         public Medicine GetMedicineById(int MedicineID);
-        public Medicine InsertMedicine(Medicine MedicineEntity,string categoryName);
+        public Medicine InsertMedicine(Medicine MedicineEntity);
         public Medicine UpdateMedicine(Medicine MedicineEntity);
-        public Medicine DeleteMedicine(int MedicineID);
+        public Medicine DeleteMedicine(Medicine medicineEntity);
     }
 }
